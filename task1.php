a23d8c33aade3273020a4dd31b9b6bdf84c67ad7
github_username <you@example.com>
2023-09-13 01:47:18 +0300

    task1

---
+++ task1.php
@@ -0,0 +1,64 @@
+<?php
+
+$firstname='nehal';
+$lastname='kamal';
+$age=23;
+
+ echo "my name is $firstname $lastname and my age is $age. <br>";
+
+echo 'welcome '.$firstname .',' .$lastname.'<br>';
+
+print('name is '.$firstname.', age is '.$age);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task 1</title>
+    <style>
+    table, th, td {
+  border:1px solid darkblue;
+  color:;
+}
+</style>
+</head>
+    <body>
+    
+    <h3>differences between echo and print :</h3>
+
+<table style="width:100%">
+<tr>
+    <th>echo</th>
+    <th>print</th>
+    
+  </tr>
+
+  <tr>
+  
+    <td>echo can accept multiple expressions</td>
+    <td>It cannot accept multiple expressions</td>
+</tr>
+<tr>
+    <td>It doesn’t return any value</td>
+    <td>It always returns the value 1</td>
+    </tr>
+<tr>
+     <td>echo is faster then print</td>
+     <td>It is slower than echo as it returns a value</td>
+     </tr>
+     <tr>
+     <td>echo  can pass multiple string separated </td>
+     <td>It cannot pass multiple arguments</td>
+     </tr>
+ 
+</table>
+<h3>some explanation for osi layers:</h3>
+
+<a href=" https://www.youtube.com/watch?v=Ilk7UXzV_Qc&list=LL&index=12&t=295s " target="_blank">show video</a>
+
+
+</body>
+</html>
